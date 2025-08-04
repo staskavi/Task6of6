d3d64eb6809d7421d2ce8e8e866435a9ee39e133
StasK <staskavi@gmail.com>
2025-08-04 23:49:19 +0300

    getchar error

--- Task6of6.c
+++ Task6of6.c
@@ -1,5 +1,37 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
+	char ch;
+	int counter_letters = 0;
+	int counter_numbers = 0;
+	int counter_other = 0;
+	printf("Enter char (for exit enter #)...");
+	ch = getchar();
+
+	if (ch == 35)
+	{
+		printf("Exit. You have entered #!");
+		return 1;
+	}
+	while (ch != 35)
+	{
+		
+		if (ch >= 97 && ch <= 122)
+		{
+			counter_letters++;
+		}
+		else if (ch >= 48 && ch <= 57)
+		{
+			counter_numbers++;
+		}
+		else
+		{
+			counter_other++;
+		}
+		printf("Enter char (for exit enter #)... ");
+		ch = getchar();
+	}
+	printf("Number of small letters is %d\n Number of numbers is %d\n Number of other symbols is %d", counter_letters, counter_numbers, counter_other);
 	return 0;
 }
